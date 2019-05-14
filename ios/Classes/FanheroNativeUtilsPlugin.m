2093214c0debccba00bf7a58c645e9c2c222f66a
Duy Bao Nguyen <bduyng@gmail.com>
2019-05-14 10:41:20 +0700

    initial commit

---
+++ ios/Classes/FanheroNativeUtilsPlugin.m
@@ -0,0 +1,20 @@
+#import "FanheroNativeUtilsPlugin.h"
+
+@implementation FanheroNativeUtilsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fanhero_native_utils"
+            binaryMessenger:[registrar messenger]];
+  FanheroNativeUtilsPlugin* instance = [[FanheroNativeUtilsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
